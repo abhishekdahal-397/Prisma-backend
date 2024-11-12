aca89880663efe292a2de1af514fc4caacba4ae0
Abhishek Dahal <abhishekdahal397@gmail.com>
2024-11-12 18:08:35 +0545

    hitesh course 1 video completed prisma regitstering users

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+//regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//cookie middleware
+app.use(cookieParser());
+const userRouter = require("./routes/userRoutes");
+app.use("/api", userRouter);
+
 app.get("/", (req, res) => {
 	res.send("Hi from youtube live");
 });
